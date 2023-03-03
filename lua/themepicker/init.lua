d4175f7bfcb867337d6caa942c1468c278fbeb74
AlejandroSuero <71392160+AlejandroSuero@users.noreply.github.com>
2023-03-03 16:13:39 +0100

    Fixed bug with bufnr on close

--- lua/themepicker/init.lua
+++ lua/themepicker/init.lua
@@ -6,15 +6,9 @@ themes:find()
 local configs = require('themepicker.configs')
 
 local M = {}
-local _is_open = false
 local themes = configs._themes
 
-M.toggle_menu = function()
-  if (_is_open) then
-    configs._actions.close(0)
-    _is_open = false
-  end
-  _is_open = true
+M.open_menu = function()
   themes:find()
 end
 return M
