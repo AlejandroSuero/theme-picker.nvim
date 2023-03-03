6de24936a694182c3ee480b94df2fab5459732b0
AlejandroSuero <71392160+AlejandroSuero@users.noreply.github.com>
2023-03-03 15:56:12 +0100

    Added installed themes and height logic

--- lua/themepicker/configs.lua
+++ lua/themepicker/configs.lua
@@ -3,53 +3,64 @@ if (not ok_pickers) then
   vim.notify('Telescope is required for this plugin to work.\nGo to https://github.com/nvim-telescope/telescope.nvim', 3)
   return
 end
-local finders = require('telescope.finders')
-local sorters = require('telescope.sorters')
-local actions = require('telescope.actions')
+local finders       = require('telescope.finders')
+local sorters       = require('telescope.sorters')
+local actions       = require('telescope.actions')
 local actions_state = require('telescope.actions.state')
 
-local M = {
+local M             = {
   _finders = finders,
   _pickers = pickers,
   _sorters = sorters,
   _actions = actions,
   _actions_state = actions_state,
+  _installed_themes = vim.fn.getcompletion('', 'color'),
 }
 
-M._set_theme = function()
+M._set_theme        = function()
   local selected = actions_state.get_selected_entry()
   vim.cmd.colorscheme(selected[1])
 end
 
-M._select_theme = function(prompt_bufnr)
+M._select_theme     = function(prompt_bufnr)
   M._set_theme()
   actions.close(prompt_bufnr)
 end
 
-M._get_next_theme = function(prompt_bufnr)
+M._get_next_theme   = function(prompt_bufnr)
   actions.move_selection_next(prompt_bufnr)
   M._set_theme()
 end
 
-M._get_prev_theme = function(prompt_bufnr)
+M._get_prev_theme   = function(prompt_bufnr)
   actions.move_selection_previous(prompt_bufnr)
   M._set_theme()
 end
+
+M._max_height       = vim.o.lines - 10
+M._max_width        = vim.o.columns - 10
+M._height           = #M._installed_themes
+M._width            = 0.5
+
+if (#M._installed_themes < M._max_height) then
+  M._height = 0.5
+end
+
 
 M.win_opts = {
   results_title    = ' Themes ',
   prompt_title     = ' Find your theme ',
   layout_strategy  = 'vertical',
   layout_config    = {
-    height = 10,
-    width = 0.5,
+    height = M._height,
+    width = M._width,
     prompt_position = 'top',
   },
   sorting_strategy = 'ascending',
 }
 
 M.opts = {
-  finder = M._finders.new_table { 'duskfox', 'github_dark', 'kanagawa' },
+  finder = M._finders.new_table(M._installed_themes),
   sorter = M._sorters.get_generic_fuzzy_sorter({}),
   attach_mappings = function(prompt_bufnr, map)
     map('i', '<CR>', M._select_theme)
